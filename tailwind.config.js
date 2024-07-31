30c0a3bdd9e0abb61591163138b86076998370a9
esmiti19 <gbesme21@gmail.com>
2024-07-31 19:09:52 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html", "./build/js/*.js"], // this is to specify for the tailwinnd to check for class that tailwind runs.
+  theme: {
+    extend: {
+      colors: {
+        papayawhip: {
+          light: '#fef4e4',
+          DEFAULT: '#ffefd5',
+          dark: '#fee5bc',
+        }
+      },
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
+  },
+  plugins: [],
+}
+//npm run tailwind -  A command that require to be runned each time you start working on your project to ensure that Tailwind CSS is watching for changes and recompiling your CSS as needed. 
